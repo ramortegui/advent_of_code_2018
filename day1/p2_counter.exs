cdda350eeade365e8019768dcd5770eb3e1313d7
Ruben Amortegui <ruben.amortegui@gmail.com>
2018-12-02 03:09:02 -0700

    Update format.

--- day1/p2_counter.exs
+++ day1/p2_counter.exs
@@ -5,11 +5,10 @@
   num = val
         |> String.trim_trailing()
         |> String.to_integer()
-  case Map.get(map, num+acc) do
-    nil -> { Map.put(map, num+acc, num+acc) ,num+acc}
+  case Map.get(map, num + acc) do
+    nil -> {Map.put(map, num+acc, num+acc), num+acc}
     res ->
       IO.puts "Found in #{res}"
       System.halt(0)
   end
-end
-)
+end)
